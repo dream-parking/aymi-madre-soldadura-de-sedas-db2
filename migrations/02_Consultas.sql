ae13995f03dd7bf4d61f72708bc80da6cbf6f0f0
Alan <desedasalan06@gmail.com>
2026-06-05 22:03:57 -0500

    Comentarios

--- migrations/02_Consultas.sql
+++ migrations/02_Consultas.sql
@@ -1,1 +1,14 @@
+--Consutal
+--1. Una consulta que muestre los recursos disponibles del sistema.
 
+--2. Una consulta que calcule totales o ingresos 
+--en un período determinado.
+
+--3. Una consulta que muestre los elementos asociados 
+--a una categoría o departamento específico.
+
+--4. Una consulta que muestre el detalle de una transacción, 
+--reserva o proceso específico.
+
+--5. Cinco consultas adicionales propuestas por el grupo 
+--que utilicen funciones de agregación, subconsultas o ambas.
